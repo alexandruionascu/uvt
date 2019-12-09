8e18b41422ddddb5af262e0414eec132a3d8f1e3
Alexandru Ionascu <alexionascu@Alexandrus-MacBook-Air.local>
2019-12-09 13:32:50 +0200

    added hw4

--- xml/hw4/hw4.py
+++ xml/hw4/hw4.py
@@ -3,11 +3,32 @@ import lxml.etree as ET
 
 
 app = Flask(__name__)
+dom = ET.parse('data.xml')
 
-@app.route("/")
-def hello():
-    dom = ET.parse('data.xml')
-    xslt = ET.parse('transform1.xml')
+def renderTransform(transform_name):
+    xslt = ET.parse(transform_name)
     transform = ET.XSLT(xslt)
     newdom = transform(dom)
     return(ET.tostring(newdom, pretty_print=True))
+
+@app.route("/")
+def T1():
+    return renderTransform('transform1.xml')
+
+@app.route("/articles")
+def T2():
+    return renderTransform('transform2.xml')
+
+@app.route("/authors")
+def T3():
+    return renderTransform("transform3.xml")
+
+@app.route("/ids")
+def T4():
+    return renderTransform("transform4.xml")
+
+@app.route("/categories")
+def T5():
+    return renderTransform("transform5.xml")
+
+    
